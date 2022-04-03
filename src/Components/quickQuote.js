b6c1ea777493d6bf48d7994c404c9731d8f27aaa
yashikanand <yashik.anand99@gmail.com>
2022-04-03 15:51:40 +1000

    Updated and fixing the homepage with the remaining styles. Added new package for country code to show the code for telephone/mobile

--- src/Components/quickQuote.js
+++ src/Components/quickQuote.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
 function QuickQuote() {
-  const [status, setStatus] = useState("Get Quote");
+  const [value, setValue] = useState();
   return (
     <>
       <form id="details">
@@ -9,46 +11,67 @@ function QuickQuote() {
           <div id="name">
             <label>
               First Name
+              <span className="required"> *</span>
               <br />
-              <input type="text" name="first-name" />
+              <input
+                type="text"
+                name="first-name"
+                required="true"
+                placeholder="First Name"
+              />
             </label>
             <label>
               Last Name
+              <span className="required"> *</span>
               <br />
-              <input type="text" name="last-name" />
+              <input
+                type="text"
+                name="last-name"
+                required="true"
+                placeholder="Last Name"
+              />
             </label>
           </div>
           <label>
             Email
             <br />
-            <input type="email" name="email" />
+            <input type="email" name="email" placeholder="Email" />
           </label>
           <label>
             Telephone/Mobile
             <br />
-            <input type="tel" name="contact" />
+            <PhoneInput name="contact" value={value} onChange={setValue} />
           </label>
         </div>
         <div id="currency-details">
           <label>
             From Currency
+            <span className="required"> *</span>
             <br />
-            <input type="text" name="from-currency" />
+            <input type="text" name="from-currency" required="true" />
           </label>
           <label>
             To Currency
+            <span className="required"> *</span>
             <br />
-            <input type="text" name="to-currency" />
+            <input type="text" name="to-currency" required="true" />
           </label>
           <label>
             Amount
+            <span className="required"> *</span>
             <br />
-            <input type="number" name="Amount" />
+            <input
+              type="number"
+              step="0.01"
+              name="Amount"
+              required="true"
+              placeholder="25000.00"
+            />
           </label>
         </div>
         <div id="submission-button">
-          <button id="submission" onClick={() => setStatus("Start New Quote")}>
-            {status}
+          <button type="submit" id="submission">
+            GET QUOTE
           </button>
         </div>
       </form>
